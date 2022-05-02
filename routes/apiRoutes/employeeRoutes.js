ee572085473677c1b093a64232c24c78c4262bb8
Matthew Parker <mattparker124@gmail.com>
2022-05-02 01:01:39 -0500

    added put request for employee role editing

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -44,4 +44,31 @@ router.post('/employee', ({ body }, res) => {
     });
 });
 
+router.put('/employee/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'role_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `UPDATE employees SET role_id = ? WHERE id = ?`;
+    const params = [req.body.role_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 module.exports = router;
